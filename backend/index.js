7a2a591955c126964b2cc8416a5f86498938bd04
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-25 05:57:30 +0300

    MongoDB connection approved

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
+// Requirements
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000
 
-const app = express()
+dotenv.config();
 
-dotenv.config()
-
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-
-
-app.get('/api/set', async(req,res) => {
-    console.log('test')
+// MONGODB CONNECTION
+mongoose.connect(process.env.MONGODB_CONNECTION)
+.then(() => {
+    console.log('MongoDB Connected');
 })
+.catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+});
 
-app.listen(7000, () => {console.log(`started with  `)})
+// Express configuration
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Test Method
+app.get('/api/set/test', async (req, res) => {
+    console.log('test');
+    res.send('Test endpoint');
+});
+
+
+// Listen config
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
