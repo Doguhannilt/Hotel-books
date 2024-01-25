a9aae7e0f548b97b9c036b4004788b6bc6ce3c0c
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-25 20:21:28 +0300

    Layouts: header,hero,footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 
 import './App.css'
+import Layout from './layouts/Layout'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <div>
+    <Router>
+      <Routes>
+       <Route path="/" element={<Layout></Layout>}/>
+       <Route path="/search"  element={<>Search Page</>}/>
+       <Route path="*"  element={<Navigate to = "/" />}/>
 
-    </div>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className = "bg-sky-500/90 pb-16">
+      <div className = "container mx-auto flex flex-col gap-2">
+        <h1 className = "text-5xl text-white font-bold">You are going to travel to...</h1>
+        <p className = "text-2xl text-white">for your dream vacation</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
