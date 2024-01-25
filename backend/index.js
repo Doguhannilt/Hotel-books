f8185f71891b213cf1fdace1d19b67fdabc4f1b7
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-25 05:28:16 +0300

    Backend structure

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+const app = express()
+
+dotenv.config()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+
+app.get('/api/set', async(req,res) => {
+    console.log('test')
+})
+
+app.listen(7000, () => {console.log(`started with 7000 `)})
