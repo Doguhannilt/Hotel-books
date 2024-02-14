f29b12a03a9d72367639e4d35941d7fb4a5565ea
Doğuhan İlter <doguhannilt@gmail.com>
2024-02-14 07:32:53 +0300

    Express validator is added, required fields marked for user

--- backend/src/routes/Users.js
+++ backend/src/routes/Users.js
@@ -2,14 +2,28 @@ const express = require('express');
 const UserModel = require('../models/model');
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+router.post("/register",[
+    // ! It will check if parameters exists, they're not, the message will shows up
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({min:6})
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({message:errors.array()})
+    }
+
     try {
         const user = await UserModel.findOne({
             email: req.body.email,
         });
+            console.log("🚀 ~ router.post ~ email:", email)
 
         if (user) {
             return res.status(400).json({ message: "User Already Exists" });
