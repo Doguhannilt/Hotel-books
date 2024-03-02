10fc365988fcd4e49952c8f59ee0774998316d55
Doğuhan İlter <doguhannilt@gmail.com>
2024-03-02 15:03:15 +0300

    Frontend - src/pages/MyHotels.jsx finished

--- frontend/src/pages/MyHotels.jsx
+++ frontend/src/pages/MyHotels.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
+import { BsBuilding, BsMap } from "react-icons/bs";
+import {BiHotel, BiMoney, BiStar} from "react-icons/bi"
 
 const MyHotels = () => {
     axios.defaults.withCredentials = true
@@ -26,11 +28,57 @@ const MyHotels = () => {
 
   return (
     <div>
-    <div className='space-y-5'>
-      <span className='flex justify-between'>
+    <div className='space-y-5  pl-60 pr-60'>
+      <span className='flex justify-between mt-4'>
         <h1 className = "text-3xl font-bold">My Hotels</h1>
         <Link to= "add-hotel" className = "flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500">Add Hotel</Link>
       </span>
+    </div>
+      {
+        hotels.map(hotel=>(
+          <div className='mt-4 mb-4  pl-60 pr-60'>
+            <div className='bg-blue-300/50'>
+            <h3 className='font-bold text-2xl'>
+              {hotel.name}
+            </h3>
+            <span>
+              {hotel.description}
+            </span>
+
+            <div className='grid grid-cols-5 gap-2'>
+              <div className='flex'>
+                <BsMap className='mr-2 mt-1'/>
+                {hotel.city} , {hotel.country}
+              </div>
+              <div className='flex'>
+                <BsBuilding className='mr-2 mt-1'/>
+                {hotel.type} 
+              </div>
+              <div className='flex'>
+                <BiMoney className='mr-2 mt-1'/>
+                € {hotel.pricePerNight} per night
+              </div>
+              <div className='flex'>
+                <BiHotel className='mr-2 mt-1'/>
+                {hotel.adultCount} Adult, {hotel.childCount} Children
+              </div>
+              <div className='flex'>
+                <BiStar className='mr-2 mt-1'/>
+                {hotel.starRating} star rating
+              </div>
+            </div>
+          </div>
+          <span className='flex justify-end '>
+            <Link
+            className='bg-blue-600 hover:bg-blue-500 text-white font-bold h-10 w-40 text-2xl mt-4 text-center'
+            to={`/edit-hotel/${hotel._id}`}>
+              View Details
+            </Link>
+          </span>
+          </div>
+        ))
+      }
+    <div>
     </div>
     </div>
   )
